3d5aa0aedec98da08c13f36a9f552bf2da669244
Maru Di Primio <mariana.diprimio@gmail.com>
2022-07-10 02:06:35 +0200

    barre de busqueda terminada

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './styles.module.scss'
 import { ProductsData } from "../../Data/productsData";
 import { useContext } from "react";
 import { CartContext } from "../../Context/cartContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
 
 const Products = () => {
     const {addItemToCart} = useContext(CartContext)
+    const [productos, setProductos] = useState([ProductsData])
+    const [busqueda, setBusqueda] = useState("")
+   
+    const handleChange = e =>{
+        setBusqueda(e.target.value) 
+        //console.log("Busqueda: " +e.target.value);
+        filtrar(e.target.value);
+    } 
+   
+    const filtrar = (terminoBusqueda)=>{
+        var aux = ProductsData.filter((prod)=> prod.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+        );
+        setProductos(aux);
+        console.log(productos)
+
+    }
+
+
     return (
       <> 
-       <div className={styles.search}> <input className={styles.inputBuscar}
-       
-       placeholder="Búsqueda por marca o modelo" />
+       <div className={styles.search}> 
+       <input className={styles.inputBuscar}
+       value={busqueda}
+       placeholder="Búsqueda por marca o modelo" 
+       onChange={handleChange}
+       />
        <button className={styles.btnSearch}>
             <FontAwesomeIcon className={styles.icono} icon={faSearch}/>
        </button></div>
         <div className={styles.productsContainer}>
             
-           {ProductsData.map((product,i) => (
+           {productos.map((product,i) => (
             <div className={styles.product}   key={i}>
                 <img src={product.img} alt={product.name} />
-                {console.log(product.img)}
+                
                 <div>
                     <p>
                         {product.name}
